deb6faea40dad68db04f132bf726d587573bc83c
Soumyodipto68 <soumyodiptopal77@gmail.com>
2026-06-05 21:36:07 +0530

    inverted Rightangle pattern

---
+++ Patterns/Pattern2.java
@@ -0,0 +1,19 @@
+package Patterns;
+import java.util.Scanner;
+public class Pattern2 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number of rows");
+    int n = sc.nextInt();
+    if(n<=0){
+      System.out.println("You are a fucking Idiot");
+    }
+    for(int i=0;i<n;i++){
+      for(int j=i;j<n;j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    sc.close();
+  }
+}
