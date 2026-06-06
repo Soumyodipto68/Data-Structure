60b8f4ff2d243ec85b14e72fc180037a5d53d3ec
Soumyodipto68 <soumyodiptopal77@gmail.com>
2026-06-06 19:37:50 +0530

    Number patterns Right angle inverted

---
+++ Patterns/Pattern4.java
@@ -0,0 +1,22 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern4 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number of rows");
+    int n = sc.nextInt();
+    if(n<=0){
+      System.out.println("You are a fucking Idiot");
+    }
+    System.out.println("The pattern is:");
+    for(int i=1;i<=n;i++){
+      for(int j=1;j<=i;j++){
+        System.out.print(j+"");
+      }
+      System.out.println();
+    }
+    sc.close();
+  }
+}
