9a3cd06366a900722058186e3c8a8b337409d6ea
Soumyodipto68 <soumyodiptopal77@gmail.com>
2026-06-06 19:57:11 +0530

    Full Rectangle

---
+++ Patterns/Pattern5.java
@@ -0,0 +1,20 @@
+package Patterns;
+import java.util.Scanner;
+public class Pattern5 {
+  public static void main(String[] args) {
+   int n = 5;
+   Scanner sc = new Scanner(System.in);
+   System.out.println("Enter the number of rows");
+   n = sc.nextInt();
+    if(n<=0){
+      System.out.println("You are a fucking Idiot");
+    }
+    for(int i = 1; i<=n; i++){
+      for(int j =1; j<=n; j++){
+        System.out.print("* ");
+      }
+      System.out.println();
+    } 
+    sc.close();
+  }
+}
