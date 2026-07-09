eb0eaeec9f1d71492c327763534e47b6f7d031f0
Soumyodipto68 <soumyodiptopal77@gmail.com>
2026-07-09 20:46:56 +0530

    Plus Pattern done

--- Patterns/Pattern16.java
+++ Patterns/Pattern16.java
@@ -1,23 +1,23 @@
 package Patterns;
 import java.util.Scanner;
-public class Pattern15 {
+public class Pattern16 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter the Number of Rows: ");
     int n = sc.nextInt();
-    if(n<=0){
+    if (n<=0) {
       System.out.println("You are a fucking Idiot!!!");
     }
             for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                if (i == j || i + j == n + 1)
+                if (i == (n / 2 + 1) || j == 
+                                (n / 2 + 1)) {
                     System.out.print("*");
-                else
+                } else {
                     System.out.print(" ");
+                }
             }
             System.out.println();
         }
-      sc.close();  
-    }
   }
-
+}
