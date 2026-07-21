7f968760f20784cbddbabc00a4de414bd6603c59
Soumyodipto68 <soumyodiptopal77@gmail.com>
2026-07-21 22:13:11 +0530

    Add Magic Board problem description

---
+++ QNAs/13MagicBoard.py
@@ -0,0 +1,8 @@
+"""13.Problem Description"""
+
+
+"""
+Ritik wants a Magic Board,Which displays a characters for a corresponding numbers for his science Project. Help him to develop such an application.
+
+
+"""
